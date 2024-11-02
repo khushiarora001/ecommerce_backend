0b5f61422f69893f8f4ec92199637b827db36bfd
Khushi <khushi.arora@nestorbird.com>
2024-11-02 16:06:48 +0530

    add path users

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./models/user');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const authenticateToken = require('./middleware/auths');
